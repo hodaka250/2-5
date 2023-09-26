9abfa9cb8d39f169709683ffe7c7358f61d6dd60
hodaka250 <95903411+hodaka250@users.noreply.github.com>
2023-09-26 17:55:00 +0900

    test2
    2

--- test2.js
+++ test2.js
@@ -1,18 +1,18 @@
 function viewChange(){
-  if(document.getElementById('sample')){
-      id = document.getElementById('sample').value;
-      if(id == 'selected_jpn'){
-          document.getElementById('jp').style.display = "";
-          document.getElementById('en').style.display = "none";
-      }else if(id == 'selected_en'){
-          document.getElementById('jp').style.display = "none";
-          document.getElementById('en').style.display = "";
-      }
-      else if(id == 'selected_all'){
-          document.getElementById('jp').style.display = "";
-          document.getElementById('en').style.display = "";
-      }
+    if(document.getElementById('sample')){
+        id = document.getElementById('sample').value;
+        jp = document.getElementById('jp');
+        en = document.getElementById('en');
+        if(id == 'selected_jpn'){
+            jp.style.display = "";
+            en.style.display = "none";
+        }else if(id == 'selected_en'){
+            jp.style.display = "none";
+            en.style.display = "";
+        }
+        else if(id == 'selected_all'){
+            jp.style.display = "";
+            en.style.display = "";
+        }
+    }
   }
-
-window.onload = viewChange;
-}
